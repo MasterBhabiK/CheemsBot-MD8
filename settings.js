ce5d28b86952149ba018fbb62e2b4217fd889394
MasterBhabiK <108351537+MasterBhabiK@users.noreply.github.com>
2023-12-14 15:36:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['918100991567'] //ur owner number
+global.ownernomer = "919088043981" //ur owner number2
+global.ownername = "🦄Dream Guy BHABIK" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "Insta: _bhabik_pal_" //ur github or insta name
+global.location = "India, Mizoram, Kolkata" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "GOJO SATORU👽🌀"
+global.ownernumber = '918100991567'
+global.ownername = '🦄드림 가이 BHABIK'
+global.ownerNumber = ["918100991567@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@Xeon"
+global.websitex = "https://youtu.be/L_SIk5QeAU"
+global.wagc = "https://chat.whatsapp.com/DcqyVeK8JbJq8Gr3U1pKH"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.creator = "918100991567@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
